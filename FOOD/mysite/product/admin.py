28b8cab8e6b2796326e6660c95d85486053ba6f9
botree <yogendra.katewa@botreetechnologies.com>
2018-12-12 18:33:46 +0530

    FOOD added

---
+++ FOOD/mysite/product/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Product_Details,Restaurant,Feedback,Order,User_Details,Cart
+
+
+
+admin.site.register(Product_Details)
+admin.site.register(Restaurant)
+admin.site.register(Feedback)
+admin.site.register(Order)
+admin.site.register(User_Details)
+admin.site.register(Cart)
+
---
+++ FOOD/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mitjob~1\pychar~1\food\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
